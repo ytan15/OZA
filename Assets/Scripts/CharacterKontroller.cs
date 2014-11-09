5fb02266dc40c007e52aa858d05e9fc175878f99
Nick <nickolenz@comcast.net>
2014-11-08 23:49:53 -0600

    Ray Jesus has arrived. Slopes have been unbanned.

--- Assets/Scripts/CharacterKontroller.cs
+++ Assets/Scripts/CharacterKontroller.cs
@@ -10,28 +10,39 @@ public class CharacterKontroller : MonoBehaviour {
 
 	bool grounded = false;
 	public Transform groundCheck;
-	float groundRadius = 0.1f;
+	float groundRadius = 0.001f;
 	public LayerMask whatIsGround;
 	public float jumpForce = 50f;
+	public Transform sightStart, sightEnd;
+	public bool notTraversable;
 
 	// Use this for initialization
 	void Start () {
 		anim = GetComponent < Animator > ();
 	}
 	
-	// Update is called once per frame
+	//  FixedUpdate is called once per frame
 	void FixedUpdate () {
+		// Jumping
 		grounded = Physics2D.OverlapCircle (groundCheck.position, groundRadius, whatIsGround);
 		anim.SetBool ("Ground", grounded);
 
 		anim.SetFloat ("vSpeed", rigidbody2D.velocity.y);
 
 
+		//Horizontal Movement
+
+		
+
+
 		float move = Input.GetAxis ("Horizontal");
 
 		anim.SetFloat ("Speed", Mathf.Abs (move));
 
-		rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
+		if (notTraversable)
+			rigidbody2D.velocity = new Vector2 (0, rigidbody2D.velocity.y);
+		else
+			rigidbody2D.velocity = new Vector2 (move * maxSpeed, rigidbody2D.velocity.y);
 		
 		if (move > 0 && !facingRight)
 			Flip ();
@@ -42,6 +53,10 @@ public class CharacterKontroller : MonoBehaviour {
 
 	void Update()
 	{
+
+		Debug.DrawLine (sightStart.position, sightEnd.position, Color.blue); //Visual Representation of Ray
+		notTraversable = Physics2D.Linecast (sightStart.position, sightEnd.position, whatIsGround);
+
 		if (grounded && Input.GetKeyDown (KeyCode.Space))
 		{
 			anim.SetBool ("Ground",false);
