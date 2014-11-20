d5f491798954534d26aff48f08057966bc422f10
Nick <nickolenz@comcast.net>
2014-11-20 17:48:33 -0600

    Grunt work of combat system V1 has been completed. It's silly but functional. Pros and cons laid out in /_WORK/CurrentCombatProsCons.txt

--- Assets/Scripts/CombatController.cs
+++ Assets/Scripts/CombatController.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class CombatController : MonoBehaviour {
 
-	public double songValue;
+	public float songValue;
 	bool canPlay = true;
-
+	public int notesPlayed;
+	float[] randomNumbers = {1f, 3f, 4f, 6f, 7f, 6f, 3f, 3f, 7f, 3f, 8f, 6f, 9f, 3f, 9f, 7f, 2f, 6f, 2f, 7f, 1f, 6f, 9f, 2f, 7f, 4f, 3f, 6f, 8f};	//A stupid 28 array
 
 	// Use this for initialization
 	void Start () {
@@ -16,40 +17,48 @@ public class CombatController : MonoBehaviour {
 	void Update () {
 	if (canPlay && Input.GetKeyDown (KeyCode.Keypad1))  //C
 		{
-			songValue += 1;
+			songValue += (1 * randomNumbers[notesPlayed]) + Mathf.Pow(notesPlayed,2);
+			notesPlayed++;
 		}
 	if (canPlay && Input.GetKeyDown (KeyCode.Keypad2))	//D
 		{
-			songValue += 2;
+			songValue += (2 * randomNumbers[notesPlayed]) + Mathf.Pow(notesPlayed,2);
+			notesPlayed++;
 		}
 	if (canPlay && Input.GetKeyDown (KeyCode.Keypad3))  //E
 		{
-			songValue += 3;
+			songValue += (3 * randomNumbers[notesPlayed]) + Mathf.Pow(notesPlayed,2);
+			notesPlayed++;
 		}
 	if (canPlay && Input.GetKeyDown (KeyCode.Keypad4))	//F
 		{
-			songValue += 4;
+			songValue += (4 * randomNumbers[notesPlayed]) + Mathf.Pow(notesPlayed,2);
+			notesPlayed++;
 		}
 	if (canPlay && Input.GetKeyDown (KeyCode.Keypad5))  //G
 		{
-			songValue += 5;
+			songValue += (5 * randomNumbers[notesPlayed]) + Mathf.Pow(notesPlayed,2);
+			notesPlayed++;
 		}
 	if (canPlay && Input.GetKeyDown (KeyCode.Keypad6))	//A
 		{
-			songValue += 6;
+			songValue += (6 * randomNumbers[notesPlayed]) + Mathf.Pow(notesPlayed,2);
+			notesPlayed++;
 		}
 	if (canPlay && Input.GetKeyDown (KeyCode.Keypad7))  //B
 		{
-			songValue += 7;
+			songValue += (7 * randomNumbers[notesPlayed]) + Mathf.Pow(notesPlayed,2);
+			notesPlayed++;
 		}
 	if (canPlay && Input.GetKeyDown (KeyCode.Keypad8))	//High C
 		{
-			songValue += 8;
+			songValue += (8 * randomNumbers[notesPlayed]) + Mathf.Pow(notesPlayed,2);
+			notesPlayed++;
 		}	
-	
-	
-	
-	
-	
+
+	if (notesPlayed >= 29)
+		notesPlayed = 0;
+
+
 	}
 }
