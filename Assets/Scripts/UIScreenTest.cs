58aa896dcd5eb95caa3434e5e8ed7bbd8f817a97
Nick <nickolenz@comcast.net>
2015-01-03 18:49:31 -0600

    Began implementation of menu stuff.

---
+++ Assets/Scripts/UIScreenTest.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIScreenTest : MonoBehaviour {
+
+	public Canvas uiCanvas;
+	public Transform menuObject;
+	public LayerMask menuOpener;
+	public float openRadius = 10f;
+	bool menuCanBeOpened = false;
+	bool menuCurrentlyOpen = false;
+
+	// Use this for initialization
+	void Start () {
+		uiCanvas.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		menuCanBeOpened = Physics2D.OverlapCircle (menuObject.position, openRadius, menuOpener);
+		if (menuCanBeOpened && Input.GetKeyDown (KeyCode.E)) 	// Opening menu
+		{
+			uiCanvas.enabled = true;
+			menuCurrentlyOpen = true;
+		}
+		if (menuCurrentlyOpen && Input.GetKeyDown (KeyCode.Escape)) 	//Closing menu
+		{
+			uiCanvas.enabled = false;
+			menuCurrentlyOpen = false;
+		}
+	
+	
+	}
+
+
+}
