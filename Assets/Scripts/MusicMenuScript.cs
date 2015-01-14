654d03c0dd19e17e30bbbd7e7b782c5dace3fa6f
Nick <nickolenz@comcast.net>
2015-01-14 14:34:13 -0600

    Tiny amount of audio work.

--- Assets/Scripts/MusicMenuScript.cs
+++ Assets/Scripts/MusicMenuScript.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class MusicMenuScript : MonoBehaviour {
 
-	public Canvas mainCanvas;
+	public Canvas mainCanvas;				//All of the different menu sections are divided based on their canvas.
 	public Canvas musicMenuCanvas;
 	public Canvas instrumentsMenuCanvas;
 	public Canvas scalesMenuCanvas;
 	public Canvas songsMenuCanvas;
-	public Transform menuObject;
-	public LayerMask menuOpener;
-	public float openRadius = 0.25f;
+	public Transform menuObject;			//The item that opens the menu
+	public LayerMask menuOpener;			//Player
+	public float openRadius = 0.25f;		//How close the player must be
 	bool menuCanBeOpened = false;
 	bool menuCurrentlyOpen = false;
+
+	public AudioClip clickSound;
+	public AudioClip menuOpenSound;
+	public AudioClip menuCloseSound;
 
 	// Use this for initialization
 	void Start () {
@@ -27,11 +31,13 @@ public class MusicMenuScript : MonoBehaviour {
 			KillAllMenus ();	//Ensures a fresh menu state
 			mainCanvas.enabled = true;
 			musicMenuCanvas.enabled = true;
+			MenuOpenSound ();
 			menuCurrentlyOpen = true;
 		}
 		if (menuCurrentlyOpen && Input.GetKeyDown (KeyCode.Escape)) 	//Closing menu
 		{
 			KillAllMenus ();
+			MenuCloseSound ();
 		}
 	
 	
@@ -44,5 +50,15 @@ public class MusicMenuScript : MonoBehaviour {
 		songsMenuCanvas.enabled = false;
 		menuCurrentlyOpen = false;
 	}
+	public void ClickSound () {
+		AudioSource.PlayClipAtPoint (clickSound, menuObject.position);
+	}
+	public void MenuOpenSound () {
+		AudioSource.PlayClipAtPoint (menuOpenSound, menuObject.position);
+	}
+	public void MenuCloseSound () {
+		AudioSource.PlayClipAtPoint (menuCloseSound, menuObject.position);
+	}
+
 
 }
